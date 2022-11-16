b652c166deef3072b62ed1655968c4b73933ab10
KUMAR PARTH <104761782+shivesh2401@users.noreply.github.com>
2022-11-16 19:26:56 +0530

    Update CONTACT.js

--- CONTACT.js
+++ CONTACT.js
@@ -1,1 +1,13 @@
+<script type = "text/javaScript">
+      
+        function openForm() 
+           {
+           document.getElementById("popupForm").style.display = "block";
+           }
 
+        function closeForm() 
+           {
+            document.getElementById("popupForm").style.display = "none";
+           }
+      
+        </script>
